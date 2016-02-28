fd15d8413da0c188d1e7f71369c19d3a63b17651
moizwasti <moizwasti@gmail.com>
2016-02-28 15:36:35 +0000

    Final final

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,30 +15,30 @@
 //= require turbolinks
 //= require_tree .
 
-var $container	= $('#ib-container'),
-	$articles	= $container.children('article'),
-	timeout;
+// var $container	= $('#ib-container'),
+// 	$articles	= $container.children('article'),
+// 	timeout;
 
-$articles.on( 'mouseenter', function( event ) {
+// $articles.on( 'mouseenter', function( event ) {
 		
-	var $article	= $(this);
-	clearTimeout( timeout );
-	timeout = setTimeout( function() {
+// 	var $article	= $(this);
+// 	clearTimeout( timeout );
+// 	timeout = setTimeout( function() {
 		
-		if( $article.hasClass('active') ) return false;
+// 		if( $article.hasClass('active') ) return false;
 		
-		$articles.not($article).removeClass('active').addClass('blur');
+// 		$articles.not($article).removeClass('active').addClass('blur');
 		
-		$article.removeClass('blur').addClass('active');
+// 		$article.removeClass('blur').addClass('active');
 		
-	}, 75 );
+// 	}, 75 );
 	
-});
+// });
 
-$container.on( 'mouseleave', function( event ) {
+// $container.on( 'mouseleave', function( event ) {
 	
-	clearTimeout( timeout );
-	$articles.removeClass('active blur');
+// 	clearTimeout( timeout );
+// 	$articles.removeClass('active blur');
 	
-});
+// });
 
