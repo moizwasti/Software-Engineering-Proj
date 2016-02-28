22cf7d07d71ddf24b085f84208495d2bfa7b9ca5
moizwasti <moizwasti@gmail.com>
2016-02-28 14:16:25 +0000

    Final commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,31 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+var $container	= $('#ib-container'),
+	$articles	= $container.children('article'),
+	timeout;
+
+$articles.on( 'mouseenter', function( event ) {
+		
+	var $article	= $(this);
+	clearTimeout( timeout );
+	timeout = setTimeout( function() {
+		
+		if( $article.hasClass('active') ) return false;
+		
+		$articles.not($article).removeClass('active').addClass('blur');
+		
+		$article.removeClass('blur').addClass('active');
+		
+	}, 75 );
+	
+});
+
+$container.on( 'mouseleave', function( event ) {
+	
+	clearTimeout( timeout );
+	$articles.removeClass('active blur');
+	
+});
+
